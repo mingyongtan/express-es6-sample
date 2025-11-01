85e17293e6dfe7ba6ac190b97ddbdd9a396fc157
Ming Yong Tan <65392487+mingyongtan@users.noreply.github.com>
2025-11-01 20:31:56 +0800

    Remove server setup code from app.js

--- server/app.js
+++ server/app.js
@@ -17,21 +17,4 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-// Serve static files
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Fallback to index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 export default app;
